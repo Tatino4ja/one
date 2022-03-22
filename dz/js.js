5d5b04d67826fa14e94735164c5320e4a743a80c
Tatino4ja <Tatino4ja@gmail.com>
2022-03-22 08:49:56 +0200

    Initial commit

---
+++ dz/js.js
@@ -0,0 +1,4 @@
+// let array = [5, 12, 9, 5, 8, 7, 9, 35, 26, 10]
+// let result = array[0] +array [1] +array [2] + array [3] + array [4] +array [5] +array [6] +array[7] + array[8] +array [9]
+// document.write(result)
+
